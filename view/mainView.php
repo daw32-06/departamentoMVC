1e051945716e05aa7dbaa62fe2bff908c591b93f
Juan José Rubio Iglesias <admin@tallernt.es>
2017-02-16 10:34:22 +0100

    Añadida lupa de buscar a las webfonts

--- view/mainView.php
+++ view/mainView.php
@@ -1,3 +1,15 @@
+<form action="index.php?location=login" method="post" name="buscarDpto">
+    <span class="icon-user-o icon"></span><input id="usuario" name="usuario" type="text" placeholder="Usuario">
+    <br>
+    <span class="icon-key icon"></span><input id="password" name="password" type="password" placeholder="Contraseña" >
+    <br>
+    <!-- Aqui mostraremos los mensajes de error -->
+    <div id="msg"></div>
+
+    <input type="submit" name="enviar" value="ENVIAR" class="bgblue">
+
+</form>
+
 <?php
     // Esta es la vista de la pagina principal que se mostrara nada mas hacer login
 
